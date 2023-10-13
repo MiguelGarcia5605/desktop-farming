fb0e9e43384d85ffab1c562f82559045b40a8640
MiguelGarcia5605 <miguelgarc5605@gmail.com>
2023-10-13 10:25:43 -0500

    develop Commit

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 import pygame
-from world.world_grid import Grid, Tile
-from character_controller.player import Player
+from world.world_grid import Grid, Tile, Player
 
 pygame.init()
 screen = pygame.display.set_mode((500, 500))
 pygame.display.set_caption('Desktop Farming')
 clock = pygame.time.Clock()
 
+test_grid = Grid(5, 5, (0, 0, 0), 1, 25, screen)
 
-test_grid = Grid(20, 20, (0, 0, 0), 3, 25, screen)
-test_tile = Tile(test_grid, 1, 3, 25, 25, "sprites/Dirt/plain_dirt.png")
-test_tile = Tile(test_grid, 1, 4, 25, 25, "sprites/Dirt/plain_dirt.png")
-test_tile = Tile(test_grid, 2, 0, 25, 25, "sprites/Dirt/plain_dirt.png")
-test_tile = Tile(test_grid, 1, 1, 25, 25, "sprites/Dirt/plain_dirt.png")
-test_tile = Tile(test_grid, 2, 4, 25, 25, "sprites/Dirt/plain_dirt.png")
-test_tile = Tile(test_grid, 1, 0, 25, 25, "sprites/Dirt/plain_dirt.png")
-test_tile = Tile(test_grid, 1, 2, 25, 25, "sprites/Dirt/plain_dirt.png")
-#test_player = Player("sprites/player/player_template.png", 20, 20, screen)
-#test_player.set_position(0, 0)
+test_tile1 = Tile(test_grid, 2, 2, 25, 25, "sprites/Dirt/plain_dirt.png")
+test_tile2 = Tile(test_grid, 2, 3, 25, 25, "sprites/Dirt/plain_dirt.png")
+test_tile3 = Tile(test_grid, 3, 2, 25, 25, "sprites/Dirt/plain_dirt.png")
+test_tile4 = Tile(test_grid, 3, 3, 25, 25, "sprites/Dirt/plain_dirt.png")
+    
+test_player = Player(test_grid, 2, 2, 20, 20, "sprites/player/player_template.png")
 
-print(test_grid.grid)
+print(test_grid.grid[0, 0])
 
 program_is_running = True
 while program_is_running:
@@ -30,7 +26,7 @@ while program_is_running:
     screen.fill((101, 163, 96))
     
     test_grid.show_grid_outline()
-    #test_player.show_player()
+    test_grid.show_grid()
     
     pygame.display.flip()
     
--- world/world_grid.py
+++ world/world_grid.py
@@ -9,32 +9,40 @@ class Grid:
         self.outline_thickness = outline_thickness
         self.tile_size = tile_size
         self.screen = screen
-
         self.grid = numpy.empty((self.columns, self.rows), dtype=object)
-    
+        for c in range(self.columns):
+            for r in range(self.rows):
+                self.grid[r, c] = []
+
+
     def show_grid_outline(self):
         for c in range(self.columns):
             for r in range(self.rows):
-                if self.grid[r, c] != None:
-                    tile = self.grid[r, c]
-                    self.screen.blit(tile.sprite, tile.sprite_rect)
-                pygame.draw.rect(self.screen, self.outline_color, pygame.Rect(\
-                    c * self.tile_size, r * self.tile_size, self.tile_size, self.tile_size), self.outline_thickness)
-                    
-                
+                pygame.draw.rect(
+                    self.screen, 
+                    self.outline_color, 
+                    pygame.Rect(c * self.tile_size, r * self.tile_size, self.tile_size, self.tile_size), 
+                    self.outline_thickness
+                )
+
+    def show_grid(self):
+        for c in range(self.columns):
+            for r in range(self.rows):
+                for obj in self.grid[r, c]:
+                    self.screen.blit(obj.sprite, obj.sprite_rect)
+
 class Tile:
     def __init__(self, grid, grid_position_x, grid_position_y, sprite_width, sprite_height, sprite_path):
-        self.grid = grid
-        self.grid.grid[grid_position_x, grid_position_y] = self
-        pygame.sprite.Sprite.__init__(self)
-        self.grid_position_x = grid_position_x
-        self.grid_position_y = grid_position_y
-        self.sprite_width = sprite_width
-        self.sprite_height = sprite_height
+        self.grid = grid.grid
+        self.grid[grid_position_x, grid_position_y].append(self)
         self.sprite = pygame.image.load(sprite_path)
         self.sprite_rect = self.sprite.get_rect()
-        self.sprite_rect.center = (self.sprite_width // 2, self.sprite_height // 2)
-        self.sprite_rect.update(self.grid_position_x * self.grid.tile_size,\
-                                self.grid_position_y * self.grid.tile_size,\
-                                self.sprite_width, self.sprite_height)
-         
+        self.sprite_rect.topleft = (grid_position_x * grid.tile_size, grid_position_y * grid.tile_size)
+
+class Player:
+    def __init__(self, grid, grid_position_x, grid_position_y, sprite_width, sprite_height, sprite_path):
+        self.grid = grid.grid
+        self.grid[grid_position_x, grid_position_y].append(self)
+        self.sprite = pygame.image.load(sprite_path)
+        self.sprite_rect = self.sprite.get_rect()
+        self.sprite_rect.topleft = (grid_position_x * grid.tile_size, grid_position_y * grid.tile_size)
